edaec11bda4ae1eadfca096a2b85b36cb3efa7cb
mi4seven <mi4seven@gmail.com>
2020-11-27 00:07:30 +0200

    Add CRUD Operations, Error Messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,43 @@
 class UsersController < ApplicationController
+
   # GET /users/new
   def new
     @user = User.new
-  end 
+  end
 
   # POST /users
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
   end  
+
+  # PATCH/PUT /authors/1
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+        
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
   
 end
